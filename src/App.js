b818bcbe012bacaf0a8bc4c9d287683cb074aa18
crochetcode <fred.davison@outlook.com>
2022-07-31 12:58:32 -0500

    :sparkles: add home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './reset.css';
-import {Panel} from './components/panel';
 import {Routes, Route} from 'react-router-dom';
-import {Parallax} from './examples/parallax';
 import styles from './styles/App.module.css';
+import {Parallax} from './examples/parallax';
+import {Home} from './Home';
+import {Panel} from './components/panel';
 
 export const App = () => {
   return (
     <div className={styles.main}>
       <Panel />
       <Routes>
-        <Route exact path='/' element={<Parallax />} />
+        <Route exact path='/' element={<Home />} />
         <Route path='/parallax' element={<Parallax />} />
       </Routes>
     </div>
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import styles from './styles/home.module.css';
+
+export const Home = () => {
+  return (
+    <div className={styles.home}>
+      <div className={styles.title}>
+        <h1>css examples</h1>
+      </div>
+    </div>
+  );
+};
