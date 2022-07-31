971392a9094ba6c3f46bfb28538e82b742a3297e
crochetcode <fred.davison@outlook.com>
2022-07-31 12:42:18 -0500

    :recycle: refactor with styled-jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import './reset.css';
-import styled from 'styled-components';
 import {Panel} from './components/panel';
 import {Routes, Route} from 'react-router-dom';
 import {Parallax} from './examples/parallax';
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-family: 'Arial Rounded MT Bold', sans-serif;
-  box-sizing: border-box;
-`;
+import styles from './styles/App.module.css';
 
 export const App = () => {
   return (
-    <StyledDiv>
+    <div className={styles.main}>
       <Panel />
       <Routes>
         <Route exact path='/' element={<Parallax />} />
         <Route path='/parallax' element={<Parallax />} />
       </Routes>
-    </StyledDiv>
+    </div>
   );
 };
--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,12 @@
-import styled from 'styled-components';
 import {Link} from 'react-router-dom';
-
-const StyledHeader = styled.header`
-  height: 50px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-bottom: 1px solid black;
-
-  h1 {
-    font-size: 2rem;
-  }
-
-  &:hover {
-    background: whitesmoke;
-  }
-`;
+import styles from '../styles/header.module.css';
 
 export const Header = () => {
   return (
     <Link to=''>
-      <StyledHeader>
+      <div className={styles.header}>
         <h1>css examples</h1>
-      </StyledHeader>
+      </div>
     </Link>
   );
 };
